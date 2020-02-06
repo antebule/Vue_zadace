3b7d2ec50b956126867b2e459f1b9555303602f4
Ante Bule <ante.bule@fpmoz.sum.ba>
2020-02-06 16:07:59 +0100

    added zadaca3 - electron

---
+++ zadaca3/src/renderer/components/Dice.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="rollDice()">Click to roll a Dice and see what you've got!</button>
+    <br>
+    <img v-if="rolledNumber===6" src="https://clipartart.com/images/6-dice-number-clipart-3.jpg">
+    <img v-else-if="rolledNumber===2" src="https://lh3.googleusercontent.com/proxy/rTdZtPBUBjHYAmgf5UbIm3vZuFAygfRfP4AdINWjU-10WL5YiT6CPFMEKgVKZ97I6EBaUOEexVl_EI1ZrdG5Fp6Ri2PLTFDYEpMmD0GYX6irAFK3d3in87E78z_R5bqdjT-JwF8">
+    <img v-else-if="rolledNumber===3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZDeAwNhgh42xn-V5gvFfjPbfoWIWGOFey2AquwRcH4NcKXzdQ">
+    <img v-else-if="rolledNumber===4" src="https://webstockreview.net/images/dice-clipart-individual-3.png">
+    <img v-else-if="rolledNumber===1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSytlmF4-Z6Du6hmswsq8VV_XYgcjhGLjVsVXfFyfB-mLXYRyNr">
+    <img v-else-if="rolledNumber===5" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRHAVA7xTMLr858puWvBHj5sQpWE5ZdeNBReR1297fLLjMbHU7X">
+  </div>
+</template>
+<script>
+//   const axios = require('axios')
+export default {
+  name: 'Dice',
+  data: () => {
+    return {
+      rolledNumber: 0
+    }
+  },
+  methods: {
+    rollDice () {
+      this.rolledNumber = Math.round(Math.random() * 5 + 1)
+    }
+  }
+}
+</script>
+<style scoped>
+img{
+  margin-left: 100px;
+  margin-top: 15px;
+  width: 60px;
+  height: 60px;
+}
+</style>
---
+++ zadaca3/src/renderer/components/Maps.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5803.021639105553!2d17.793136494420956!3d43.34542448069827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134b43baf0325777%3A0xec9b6e4c7d5a9bfc!2sSveu%C4%8Dili%C5%A1te%20u%20Mostaru!5e0!3m2!1shr!2sba!4v1581000172760!5m2!1shr!2sba" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Maps',
+  data: () => {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+</style>
